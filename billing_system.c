e32fdf0201942200822f95bfb3c3527a60da65b5
Vaishnavi Chirmade <vaishnavi.chirmade@gmail.com>
2025-04-04 20:48:41 +0530

    Restaurant_Billing_System

--- billing_system.c
+++ billing_system.c
@@ -1,5 +1,51 @@
 #include<stdio.h>
+#include<string.h>
 
+
+typedef struct 
+{
+        char customer[50];
+        char date[50];
+        int item_count;
+        float total;
+}invoice;
+
+
+typedef struct
+{
+        char name[50];
+        float price;
+}menu_item;
+
+
+
+//menu list
+menu_item menu[] = 
+{
+        {"Pizza", 110.00}, {"Burger", 40.00}, {"Pasta", 140.00}, {"Sandwich", 60.00},
+        {"Coffee", 25.00}, {"French Fries", 50.00}, {"Garlic Bread", 15.00}, 
+        {"Pan Cake", 100.50}, {"Ice-cream", 80.00}, {"Cold-Drinks", 20.00}
+};
+    
+
+int menu_size = sizeof(menu) / sizeof(menu[0]);
+
+
+//to display menu list
+void display_menu()
+{
+        printf("------ MENU LIST ------\n\n");
+
+        for(int i = 0; i < menu_size; i++)
+        {
+                printf("%d. %s - $%.2f\n", i + 1, menu[i].name, menu[i].price);
+        }
+
+        printf("-------------------------\n");
+}
+
+
+//main
 int main()
 {
     int choice;
